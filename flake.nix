55586f06304f068a4f8f74484e3155efe079609e
silvarc141 <silvarc141@users.noreply.github.com>
2025-11-24 15:09:46 +0100

    include pkgs in reapack funcs

--- flake.nix
+++ flake.nix
@@ -15,72 +15,65 @@
     flake-utils,
     nixpkgs,
     nix-utils,
-  }: let
-    mkReaPackPackage = {
-      pkgs,
-      name,
-      entry,
-      version ? "latest",
-    }: let
-      files = entry.${version};
-
-      installFile = file: let
-        source = pkgs.fetchurl {
-          inherit (file) url sha256;
-        };
-        target = "$out/${file.path}";
-      in ''
-        mkdir -p "$(dirname "${target}")"
-        ln -s "${source}" "${target}"
-      '';
-
-      installCommands = builtins.map installFile files;
-    in
-      pkgs.stdenv.mkDerivation {
-        name = pkgs.lib.strings.sanitizeDerivationName name;
-        inherit version;
-
-        dontUnpack = true;
-
-        installPhase = ''
-          mkdir -p $out
-          ${builtins.concatStringsSep "\n" installCommands}
-        '';
-      };
-
-    mkReaPackIndex = {
-      pkgs,
-      jsonPath,
-    }: let
-      entries = builtins.fromJSON (builtins.readFile jsonPath);
-      buildEntry = name: entry:
-        mkReaPackPackage {
-          inherit pkgs name entry;
-        };
-    in
-      pkgs.lib.mapAttrs buildEntry entries;
-  in
-    {
-      lib = {
-        inherit mkReaPackPackage mkReaPackIndex;
-      };
-    }
-    // flake-utils.lib.eachDefaultSystem (system: let
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
       utils = nix-utils.legacyPackages.${system};
       pkgs = nixpkgs.legacyPackages.${system};
+
       generate-reapkgs = pkgs.writeShellApplication {
         name = "generate-reapkgs";
         runtimeInputs = [
           pkgs.curl
           pkgs.nix
         ];
-        text = let 
+        text = let
           script = utils.writeNuScriptBin "generate-reapkgs" (builtins.readFile ./generate-reapkgs.nu);
         in ''${pkgs.lib.getExe script} "$@"'';
       };
+
+      mkReaPackPackage = {
+        name,
+        entry,
+        version ? "latest",
+      }: let
+        files = entry.${version};
+
+        installFile = file: let
+          source = pkgs.fetchurl {
+            inherit (file) url sha256;
+          };
+          target = "$out/${file.path}";
+        in ''
+          mkdir -p "$(dirname "${target}")"
+          ln -s "${source}" "${target}"
+        '';
+
+        installCommands = builtins.map installFile files;
+      in
+        pkgs.stdenv.mkDerivation {
+          name = pkgs.lib.strings.sanitizeDerivationName name;
+          inherit version;
+
+          dontUnpack = true;
+
+          installPhase = ''
+            mkdir -p $out
+            ${builtins.concatStringsSep "\n" installCommands}
+          '';
+        };
+
+      mkReaPackIndex = jsonPath: let
+        entries = builtins.fromJSON (builtins.readFile jsonPath);
+        buildEntry = name: entry: mkReaPackPackage {inherit pkgs name entry;};
+      in
+        pkgs.lib.mapAttrs buildEntry entries;
+
     in {
       formatter = pkgs.alejandra;
       packages.default = generate-reapkgs;
       legacyPackages = {inherit generate-reapkgs;};
+      lib = {
+        inherit mkReaPackPackage mkReaPackIndex;
+      };
     });
 }
