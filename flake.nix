cab7bf89f224997ff52f6403fbc66029a9326eaf
silvarc141 <silvarc141@users.noreply.github.com>
2025-12-02 22:10:56 +0100

    update to 25.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
   outputs = {
